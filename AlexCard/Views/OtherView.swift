91e7affb6f64f7b9de996d994d7e199950985648
Alex <raku@crtx.ru>
2021-08-03 19:36:38 +0700

    new view has added

---
+++ AlexCard/Views/OtherView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherView.swift
+//  AlexCard
+//
+//  Created by Алексей Раку on 03.08.2021.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OtherView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherView()
+    }
+}
